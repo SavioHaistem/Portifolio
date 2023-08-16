7862901e2387d573006b3830b2444475b250c61d
SavioHaistem <savio.dantas.5858@gmail.com>
2023-08-16 20:28:49 -0300

    fix: person width responsiveness

--- src/components/myPerson.vue
+++ src/components/myPerson.vue
@@ -5,15 +5,21 @@
 <style>
    img.avatar {
       position: absolute;
-      width: 300px;
+      width: 400px;
       bottom: -50%;
       left: 0;
       z-index: -1;
-      height: 630px;
+      height: 830px;
    }
    @media screen and (max-width: 664px) {
       img.avatar {
          bottom: -60%;
+      }
+   }
+   @media screen and (max-width: 1196px) {
+      img.avatar {
+         height: 630px;
+         width: 300px;
       }
    }
 </style>
