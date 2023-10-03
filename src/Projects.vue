4a918122f53224f58a35a3d28515008008fd8b9f
SavioHaistem <savio.dantas.5858@gmail.com>
2023-10-02 23:27:14 -0300

    fix(style): responsiveness

--- src/Projects.vue
+++ src/Projects.vue
@@ -162,7 +162,7 @@ ul.projects-list > li.project-item > img.project-image {
 
 @media only screen and (max-width: 768px) {
   ul.projects-list > li.project-item > div.tecnologies-list {
-    transform: translateY(-103px);
+    transform: translateY(-104px);
   }
 }
 </style>
